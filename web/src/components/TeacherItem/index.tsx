bd22dab947d9f63146d1fddb43255be4b252c98b
VictorKengoo <victor.kengoo.kodama@accenture.com>
2020-08-04 23:59:31 -0300

    Dia 02: Olhando as oportunidades

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+import './styles.css'
+
+function TeacherItem() {
+    return (
+        <article className="teacher-item">
+            <header>
+                <img
+                    src="https://avatars2.githubusercontent.com/u/55894232?s=460&u=988d76189e00f291454c792d105a7147b0b23ee7&v=4"
+                    alt="Victor Kodama"
+                />
+                <div>
+                    <strong>Victor Kodama</strong>
+                    <span>Química</span>
+                </div>
+            </header>
+
+            <p>
+                Entusiasta das melhores tecnologias de química avançada.
+                <br /><br />
+                Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas
+                através de experiências. Mais de 200.000 pessoas já passaram por uma das
+                minhas explosões.
+            </p>
+
+            <footer>
+                <p>
+                    Preço/ hora
+                    <strong>R$ 100,00</strong>
+                </p>
+
+                <button type="button">
+                    <img src={whatsappIcon} alt="Whatsapp" />
+                    Entrar em contato
+                </button>
+            </footer>
+        </article>
+    );
+}
+
+export default TeacherItem
